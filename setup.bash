b8fb9aafc136b125a72765264cd5802c75a85f7a
zaininnari <zaininnari@gmail.com>
2020-03-17 08:36:24 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-url='https://raw.githubusercontent.com/bellflower2015/pspkg/master/bin/pspkg'
+url='https://raw.githubusercontent.com/zaininnari/pspkg/master/bin/pspkg'
 tmpfile=$(mktemp -t pspkg)
 curl -L $url -o $tmpfile
 bash $tmpfile --noask
